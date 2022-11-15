542323adb18380b5edde11850c33c3ef0d9b525e
Sunnyspp <sunnyspp2013@gmail.com>
2022-11-15 14:27:01 -0800

    Added Simple_shell

---
+++ general.h
@@ -0,0 +1,23 @@
+#ifndef GENERAL_H
+#define GENERAL_H
+
+#define _TRUE            1
+#define _FALSE           0
+
+#define STDIN            0
+#define STDOUT           1
+#define STDERR           2
+
+#define NON_INTERACTIVE  0
+#define INTERACTIVE      1
+
+#define PERMISSIONS      1
+#define NON_PERMISSIONS -1
+
+#define _FILE            10
+#define NON_FILE         -10
+
+#define _ENOENT          "No such file or directory"
+#define _EACCES          "Permission denied"
+#define _CMD_NOT_EXISTS  "not found"
+#define _ILLEGAL_NUMBER  "Illegal number"
